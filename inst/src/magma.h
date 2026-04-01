85b0342fc21f71a04652216f7393f9d2f50b8217
sounkou-bioinfo <sounkoutoure@gmail.com>
2026-04-01 15:27:02 +0200

    install magma from source

--- inst/src/magma.h
+++
@@ -1,6 +0,0 @@
-/** Copyright (C) 2014-2020 by Christiaan de Leeuw (CTG Lab, Vrije Universiteit Amsterdam), All Rights Reserved **/
-
-using namespace std;
-boost::random::mt19937 _RNG;
-Log _LOG;
-SupplementaryLog _SLOG;
--- inst/src/mathutils.cpp
+++
@@ -1,4 +0,0 @@
-/** Copyright (C) 2014-2020 by Christiaan de Leeuw (CTG Lab, Vrije Universiteit Amsterdam), All Rights Reserved **/
-
-#include "mathutils.h"
-
